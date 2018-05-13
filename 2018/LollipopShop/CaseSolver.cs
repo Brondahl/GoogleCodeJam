5a0aa52c25e7fbe20e25c3f1a120bfcde7060458
Brondahl <stixmclean@googlemail.com>
2018-05-13 12:55:51 +0100

    2018 Round 1C - LollipopShop

---
+++ 2018/LollipopShop/CaseSolver.cs
@@ -0,0 +1,83 @@
+﻿namespace LollipopShop
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+
+  public class CaseSolver
+  {
+    private static int numberOfCases;
+    private static bool[] lollipopSold = null;
+    private static int[] lollipopSeen = null;
+
+    public static void Run()
+    {
+      numberOfCases = int.Parse(Console.ReadLine());
+
+      for (int ii = 0; ii < numberOfCases; ii++)
+      {
+        var numberOfLollipopsAndCustomers = int.Parse(Console.ReadLine());
+
+        lollipopSold = new bool[numberOfLollipopsAndCustomers];
+        lollipopSeen = new int[numberOfLollipopsAndCustomers];
+
+        for (int jj = 0; jj < numberOfLollipopsAndCustomers; jj++)
+        {
+          var customerInput = Console.ReadLine();
+          var customerArray = customerInput.Split(' ').Select(int.Parse);
+          var D = customerArray.First();
+
+          if (D == 0)
+          {
+            Console.WriteLine("-1");
+            continue;
+          }
+
+          var customerLikes = customerArray.Skip(1).ToArray();
+
+          foreach (var likedLollipop in customerLikes)
+          {
+            lollipopSeen[likedLollipop]++;
+          }
+
+          var lollipopToSell = LeastSeenAvailablePermissibleLollipop(customerLikes);
+
+          if(lollipopSold[lollipopToSell])
+          { throw new Exception("Bugger"); }
+
+          lollipopSold[lollipopToSell] = true;
+
+          Console.WriteLine(lollipopToSell.ToString());
+          continue;
+        }
+      }
+    }
+
+    public static int LeastSeenAvailablePermissibleLollipop(int[] permissibleLollipop)
+    {
+      if (!permissibleLollipop.Any())
+      {
+        return -1;
+      }
+
+      var availablePermissibleLollipops = AvailableLollipops(permissibleLollipop);
+      if (!availablePermissibleLollipops.Any())
+      {
+        return -1;
+      }
+
+      return LeastSeenLollipop(availablePermissibleLollipops);
+    }
+
+    public static List<int> AvailableLollipops(int[] lollipopsToConsider)
+    {
+      return lollipopsToConsider.Where(pop => !lollipopSold[pop]).ToList();
+    }
+
+    public static int LeastSeenLollipop(List<int> lollipopsToConsider)
+    {
+      return lollipopsToConsider.Select(pop => new {Pop = pop, Seen = lollipopSeen[pop]}).OrderBy(obj => obj.Seen).First().Pop;
+    }
+
+  }
+}
---
+++ 2018/LollipopShop/README.txt
@@ -0,0 +1,8 @@
+  /*
+   * TODO:
+   *   - Apply Copy/Paste/Rename/Edit to all files and folders.
+   *   - VS 'Add Existing Project'
+   *   - References to Common and testing Frameworks
+   *   - CodeJam Reference to here.
+   *   - Program redirect to here.
+   */

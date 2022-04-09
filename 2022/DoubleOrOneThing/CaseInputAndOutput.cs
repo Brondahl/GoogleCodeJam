b406daec524f45c936a798e06978bb95aba8e4b2
Mike.McLean <Mike.McLean@softwire.com>
2022-04-09 02:02:33 +0100

    start 1

---
+++ 2022/DoubleOrOneThing/CaseInputAndOutput.cs
@@ -0,0 +1,36 @@
+namespace DoubleOrOneThing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class CaseInput
+    {
+        internal CaseInput(List<string> lines)
+        {
+            var values = lines.Single().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+
+            R = values[0];
+            C = values[1];
+        }
+
+        internal long R;
+        internal long C;
+    }
+
+    class CaseOutput
+    {
+        internal CaseOutput(string text)
+        {
+            Text = text;
+        }
+
+        internal string Text;
+
+        public override string ToString()
+        {
+            return Environment.NewLine + Text;
+        }
+    }
+
+}
